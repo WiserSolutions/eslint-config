1b2a1693da287385facf49976cdec34ce60e5e8f
Mike Carlotta <mcarlotta@users.noreply.github.com>
2022-03-09 14:10:21 -0800

    update babel parser

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 module.exports = {
-  'extends': [
-    'eslint:recommended',
-    'prettier'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'experimentalObjectRestSpread': true
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
     },
-    'ecmaVersion': 7,
-    'sourceType': 'module'
+    ecmaVersion: 8,
+    requireConfigFile: false,
+    sourceType: "module",
   },
-  'parser': 'babel-eslint',
-  'env': {
-    'node': true,
-    'es6': true
+  parser: "@babel/eslint-parser",
+  env: {
+    node: true,
+    es6: true,
   },
-  'rules': {
-    'arrow-body-style': ['error', 'as-needed'],
-    'no-shadow': 'error', // shadowing is an easy way to let babel/webpack make mistakes
-    'prefer-const': 'error',
-    'semi': ['error', 'never'],
-    'space-before-function-paren': ['error', {
-      'anonymous': 'always', // Prettier
-      'named': 'never',
-      'asyncArrow': 'always' // Prettier
-    }],
-    'require-atomic-updates': 'off' // too many false positives as of yet
-  }
-}
+  rules: {
+    "arrow-body-style": ["error", "as-needed"],
+    "no-shadow": "error", // shadowing is an easy way to let babel/webpack make mistakes
+    "prefer-const": "error",
+    semi: ["error", "never"],
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always", // Prettier
+        named: "never",
+        asyncArrow: "always", // Prettier
+      },
+    ],
+    "require-atomic-updates": "off", // too many false positives as of yet
+  },
+};
